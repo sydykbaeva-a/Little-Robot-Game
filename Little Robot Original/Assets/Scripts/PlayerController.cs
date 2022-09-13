bb84342214da21b06eb9875c8d0329a30515e2ec
sydykbaeva-a <sydykbaeva_ai@auca.kg>
2022-09-13 18:55:46 +0600

    player movement
    Player can shoot, jump
    Enemies can walk

--- Little Robot Original/Assets/Scripts/PlayerController.cs
+++ Little Robot Original/Assets/Scripts/PlayerController.cs
@@ -4,43 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float jumpForce = 200;
+    public float jumpForce;
     private Rigidbody playerRb;
-    private bool isJumping;
     private float horizontalInput;
     private float speed = 5;
     public GameObject shotPrefab;
+    public float gravityModifier;
+    public bool isOnGround = true;
     private Vector3 offset = new Vector3(1, 0, 0);
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(isJumping == false)
+        if(Input.GetKeyDown(KeyCode.UpArrow) && isOnGround == true)
         {
-            isJumping = Input.GetKeyDown(KeyCode.UpArrow);
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
         }
         
         if(Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(shotPrefab, transform.position + offset, shotPrefab.transform.rotation);
         }
-    }
-
-    void FixedUpdate()
-    {
-        if(isJumping)
-        {
-            playerRb.AddForce(Vector3.up * jumpForce);
-            isJumping = false;
-        }
 
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
-        
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        isOnGround = true;    
     }
 }
--- Little Robot Original/Assets/Scripts/SpawnManager.cs
+++ Little Robot Original/Assets/Scripts/SpawnManager.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collider : MonoBehaviour
+public class SpawnManager : MonoBehaviour
 {
+    public GameObject enemyPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(enemyPrefab, new Vector3(0,0,0), enemyPrefab.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
