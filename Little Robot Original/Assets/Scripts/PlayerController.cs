1e89302762a74e45ee262163e352f1ab5c5baf89
sydykbaeva-a <sydykbaeva_ai@auca.kg>
2022-09-29 18:11:36 +0600

    Some changes
    Color
    Better collision

--- Little Robot Original/Assets/Scripts/PlayerController.cs
+++ Little Robot Original/Assets/Scripts/PlayerController.cs
@@ -11,26 +11,32 @@ public class PlayerController : MonoBehaviour
     public GameObject shotPrefab;
     public float gravityModifier;
     public bool isOnGround = true;
-    private Vector3 offset = new Vector3(1, 0, 0);
+    private Vector3 offset = new Vector3(21, 3.5f, -2.9f);
     public bool endOfGame = false;
     public ParticleSystem jumpParticle;
     public ParticleSystem groundParticle;
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.UpArrow) && isOnGround == true)
+        if(endOfGame == false)
+        {
+            if(Input.GetKeyDown(KeyCode.UpArrow) && isOnGround == true)
         {
             groundParticle.Stop();
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             jumpParticle.Play();
             isOnGround = false;
+            animator.SetBool("isRunning", false);
+            animator.SetBool("isJumping", true);
         }
         
         if(Input.GetKeyDown(KeyCode.Space))
@@ -38,8 +44,11 @@ public class PlayerController : MonoBehaviour
             Instantiate(shotPrefab, transform.position + offset, shotPrefab.transform.rotation);
         }
 
-        //horizontalInput = Input.GetAxis("Horizontal");
-        //transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        animator.SetBool("isRunning", Input.GetAxis("Horizontal") != 0);
+        }
+        
     }
 
     private void OnCollisionEnter(Collision other) {
